809f5df61f3d55ab91a8cf0145f71a000b9cbf21
Bartek <bartekduda01@gmail.com>
2023-05-18 22:06:45 +0200

    Add CORS for working in localhost

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::io::Error;
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
+use actix_cors::Cors;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
@@ -8,7 +9,14 @@ async fn health_check() -> impl Responder {
 
 pub fn run() -> Result<Server, Error> {
     let server = HttpServer::new(|| {
+            let cors = Cors::default()
+                .allow_any_origin()
+                .allowed_methods(vec!["GET", "POST"])
+                .allow_any_header()
+                .max_age(3600);
+
             App::new()
+                .wrap(cors)
                 .route("/health_check", web::get().to(health_check))
         })
         .bind("127.0.0.1:8000")?
