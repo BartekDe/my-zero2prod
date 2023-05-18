3dc6bfb5fe121e8b4d01156e76f5e42255717f6e
Bartek <bartekduda01@gmail.com>
2023-05-18 23:02:55 +0200

    Fix starting app on port 8000

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,8 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind to random port");
-    let port = listener.local_addr().unwrap().port();
-
-    println!("http://127.0.0.1:{}", port);
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("Failed to bind on port 8000");
 
     run(listener)?.await
 
